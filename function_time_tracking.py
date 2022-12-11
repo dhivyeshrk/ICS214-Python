12dc063371de07046c0e1c73747e7deb48f1f2c3
Dhivyesh R K <dhivyesh.rk@gmail.com>
2022-12-11 18:41:39 +0530

    first commit

---
+++ function_time_tracking.py
@@ -0,0 +1,16 @@
+import time
+def countToN(n):
+    i = 0
+    while i<n:
+        i+=1
+def fun(n):
+    print(n)
+startTime = time.time()
+countToN(1000000)
+endTime = time.time()
+timeElapsed = endTime - startTime
+print(countToN.__closure__)
+print(f"Total execution time is {timeElapsed:.5f}")
+
+        
+    
